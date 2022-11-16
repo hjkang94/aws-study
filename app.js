61c984d3f3142e6ab3d80b23bea05a07cbfebe44
hjkang94 <ee1217e@gmail.com>
2022-11-16 12:04:27 +0900

    send message 변경

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world !!!!!!!');
+  res.send('Hello world ~~~~~~!!!!!!!');
 });
 app.listen(3000, () => {
   console.log('Connect 3000 port');
